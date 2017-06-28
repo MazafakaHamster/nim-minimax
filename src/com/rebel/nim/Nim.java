0f6d159e8f408ee9de0cef44332dab6673bbfcae
Viacheslav Khomytskyi <homiakua@gmail.com>
2017-06-29 02:16:29 +0300

    Initial minimax

--- src/com/rebel/nim/Nim.java
+++ src/com/rebel/nim/Nim.java
@@ -1,13 +1,17 @@
 package com.rebel.nim;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Nim {
+
+    private static Set<Node> generated = new HashSet<>();
+
     public static void main(String[] args) {
         Node node = new Node(8, 13, 7, 5, 9);
-        generateChildNodes(node).forEach(n -> System.out.println(n + " = " + nimSum(n)));
+        generateTree(node);
+        minimax(node, true);
+
+        node.getChildList().forEach(n -> System.out.println(n + " value " + n.getHeuristicValue()));
     }
 
     private static List<Node> generateChildNodes(Node node) {
@@ -29,9 +33,50 @@ public class Nim {
         return nodes;
     }
 
-    private static int nimSum(Node current) {
+    private static void generateTree(Node root) {
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.pollFirst();
+            if (generated.contains(node))
+                continue;
+            generated.add(node);
+            queue.addAll(generateChildNodes(node));
+        }
+    }
+
+    private static int minimax(Node node, boolean maximizingPlayer) {
+        if (node.isEmpty()) {
+            return heuristicEvaluation(node);
+        }
+
+        if (maximizingPlayer) {
+            int bestValue = Integer.MIN_VALUE;
+            for (Node child : node.getChildList()) {
+                int v = minimax(child, false);
+                bestValue = Math.max(bestValue, v);
+                child.setHeuristicValue(bestValue);
+            }
+            return bestValue;
+        } else {
+            int bestValue = Integer.MAX_VALUE;
+            for (Node child : node.getChildList()) {
+                int v = minimax(child, true);
+                bestValue = Math.min(bestValue, v);
+                child.setHeuristicValue(bestValue);
+            }
+            return bestValue;
+        }
+    }
+
+    private static int heuristicEvaluation(Node node) {
+        return nimSum(node) != 0 ? 1 : -1;
+    }
+
+    private static int nimSum(Node node) {
         int xor = 0;
-        for (Integer pile : current.getPiles()) {
+        for (Integer pile : node.getPiles()) {
             xor ^= pile;
         }
         return xor;
--- src/com/rebel/nim/Node.java
+++ src/com/rebel/nim/Node.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class Node {
 
     private List<Integer> piles;
+    private long depth;
+    private int heuristicValue;
 
     private Node parent;
     private List<Node> childList;
@@ -14,6 +16,7 @@ public class Node {
     public Node(Integer... piles) {
         this.piles = new ArrayList<>(Arrays.asList(piles));
         this.childList = new ArrayList<>();
+        this.depth = 0;
     }
 
     public Node(Node parent, List<Integer> piles) {
@@ -21,10 +24,23 @@ public class Node {
         parent.childList.add(this);
         this.piles = new ArrayList<>(piles);
         this.childList = new ArrayList<>();
+        this.depth = parent.depth + 1;
     }
 
     public List<Integer> getPiles() {
         return new ArrayList<>(piles);
+    }
+
+    public List<Node> getChildList() {
+        return childList;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public long getDepth() {
+        return depth;
     }
 
     public long gePilesNum() {
@@ -35,6 +51,14 @@ public class Node {
         return piles.stream().filter(i -> i > 0).count();
     }
 
+    public int getHeuristicValue() {
+        return heuristicValue;
+    }
+
+    public void setHeuristicValue(int heuristicValue) {
+        this.heuristicValue = heuristicValue;
+    }
+
     public boolean isEmpty() {
         return piles.stream().mapToInt(i -> i).sum() == 0;
     }
@@ -42,5 +66,20 @@ public class Node {
     @Override
     public String toString() {
         return piles.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node node = (Node) o;
+
+        return piles != null ? piles.equals(node.piles) : node.piles == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return piles != null ? piles.hashCode() : 0;
     }
 }
