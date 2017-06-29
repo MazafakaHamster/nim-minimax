9813f5ca3511b5e590b0386480b67a06343398be
Viacheslav Khomytskyi <vkhomytskyi@intel471.com>
2017-06-29 11:42:39 +0300

    Minimax depth, console readNum

--- src/com/rebel/nim/Nim.java
+++ src/com/rebel/nim/Nim.java
@@ -1,15 +1,19 @@
 package com.rebel.nim;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Nim {
 
-    private static Set<Node> generated = new HashSet<>();
+    private static Set<Node>      generated = new HashSet<>();
+    private static BufferedReader reader    = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
+        System.out.println(readNum("Enter num:"));
         Node node = new Node(8, 13, 7, 5, 9);
-        generateTree(node);
-        minimax(node, true);
+//        generateTree(node);
+        minimax(node, 2, true);
 
         node.getChildList().forEach(n -> System.out.println(n + " value " + n.getHeuristicValue()));
     }
@@ -46,23 +50,23 @@ public class Nim {
         }
     }
 
-    private static int minimax(Node node, boolean maximizingPlayer) {
-        if (node.isEmpty()) {
+    private static int minimax(Node node, int depth, boolean maximizingPlayer) {
+        if (depth == 0 || node.isEmpty()) {
             return heuristicEvaluation(node);
         }
 
         if (maximizingPlayer) {
             int bestValue = Integer.MIN_VALUE;
-            for (Node child : node.getChildList()) {
-                int v = minimax(child, false);
+            for (Node child : generateChildNodes(node)) {
+                int v = minimax(child, depth - 1, false);
                 bestValue = Math.max(bestValue, v);
                 child.setHeuristicValue(bestValue);
             }
             return bestValue;
         } else {
             int bestValue = Integer.MAX_VALUE;
-            for (Node child : node.getChildList()) {
-                int v = minimax(child, true);
+            for (Node child : generateChildNodes(node)) {
+                int v = minimax(child, depth - 1, true);
                 bestValue = Math.min(bestValue, v);
                 child.setHeuristicValue(bestValue);
             }
@@ -80,5 +84,20 @@ public class Nim {
             xor ^= pile;
         }
         return xor;
+    }
+
+    private static int readNum(String text) {
+        boolean finished = false;
+        int result = 0;
+        while (!finished) {
+            try {
+                System.out.print(text + " ");
+                result = Integer.parseInt(reader.readLine());
+                finished = true;
+            } catch (Exception e) {
+                System.out.println("Wrong input");
+            }
+        }
+        return result;
     }
 }
